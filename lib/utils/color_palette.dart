9425b84de181da4c540a6327dfd27250a9969f6a
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-12-06 21:27:04 +0700

    feat: create color palette class

---
+++ lib/utils/color_palette.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static final Color yellow = Color(0xfff1ca20);
+  static final Color red = Color(0xfff86cb4);
+  static final Color shadow = Color(0xff6f97b2);
+}
